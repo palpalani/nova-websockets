8e08ccc3afc114cf6f94a8ebe84690ddb59c5a1d
palPalani <palaniappan@innoppl.com>
2021-07-04 22:27:31 +0530

    Added tests

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Vemcogroup\Websockets\Tests;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
